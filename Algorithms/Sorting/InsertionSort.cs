7ae2f422b5315608bfdca659ff7c0ccf274af6a5
elliot <postsneakernet@gmail.com>
2016-11-01 18:11:13 -0400

    Implement insertion sort

---
+++ Algorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    public class InsertionSort
+    {
+        public static void Sort(int[] a)
+        {
+            for (int i = 1; i < a.Length; ++i)
+            {
+                int temp = a[i];
+                int j = i - 1;
+
+                while (j >= 0 && a[j] > temp)
+                {
+                    a[j + 1] = a[j];
+                    --j;
+                }
+
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
